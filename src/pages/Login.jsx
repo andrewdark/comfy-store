6f18ea86fce173ab8fefd39545103f2395ba9b48
andrewdark <samsonov.a@ukr.net>
2026-05-20 23:44:23 +0300

    add register form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,15 +4,15 @@ import { Form, Link } from 'react-router-dom';
 const Login = () => {
     return (
         <section className='h-screen grid place-items-center'>
-            <Form method='post' className='card p-8 bg-base-100 shadow-lg gap-2 lg:w-72'>
+            <Form method='post' className='card p-8 bg-base-100 shadow-lg gap-y-2 lg:w-96'>
                 <h4 className='text-center text-3xl font-bold'>Login</h4>
                 <FormInput label='Email:' name='identifier' type='email' />
-                <FormInput label='Password:' name='identifier' type='password' />
+                <FormInput label='Password:' name='password' type='password' />
                 <div className='mt-4'>
-                    <SubmitBtn text='login' />
+                    <SubmitBtn text='Login' />
 
                 </div>
-                <button type='button' className='btn btn-secondary btn-block'>guest login</button>
+                <button type='button' className='btn btn-secondary btn-block'>Guest Login</button>
                 <p className='text-center'>
                     Not a member yet?{' '}
                     <Link
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,29 @@
+import { FormInput, SubmitBtn } from '../components';
+import { Form, Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <section className='h-screen grid place-items-center'>
+            <Form method='post' className='card p-8 bg-base-100 shadow-lg gap-y-4 lg:w-96'>
+                <h4 className='text-center text-3xl font-bold'>Register</h4>
+                <FormInput label='Email:' name='identifier' type='email' />
+                <FormInput label='Password:' name='password' type='password' />
+                <FormInput label='Confirm Password:' name='confirm' type='password' />
+                <div className='mt-4'>
+                    <SubmitBtn text='Register' />
+                </div>
+
+                <p className='text-center'>
+                    Already a member?{' '}
+                    <Link
+                        to='/login'
+                        className='ml-2 link link-hover link-primary text-blue-500 capitalize'
+                    >
+                        login
+                    </Link>
+                </p>
+            </Form>
+        </section>
     );
 };
 
