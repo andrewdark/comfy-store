80f052255631c119881d090bdd8ce4e4abbf3953
andrewdark <samsonov.a@ukr.net>
2026-05-21 11:48:56 +0300

    add navLinks - menu-list

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
+import { NavLinks } from '../components';
 
 const NavBar = () => {
     return (
@@ -19,12 +20,14 @@ const NavBar = () => {
                         <label tabIndex={0} className='btn btn-ghost lg:hidden'>
                             <FaBarsStaggered className='h-6 w-6' />
                         </label>
-                        <ul tabIndex={0} className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52'>one two three</ul>
+                        <ul tabIndex={0} className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52'>
+                            <NavLinks />
+                        </ul>
                     </div>
                 </div>
                 <div className='navbar-center hidden lg:flex'>
                     {/* HORIZONTAL CLASSIC MENU */}
-                    <ul className='menu menu-horizontal'>one two three</ul>
+                    <ul className='menu menu-horizontal'><NavLinks /></ul>
                 </div>
                 <div className='navbar-end'>
                     {/* THEME ICONS */}
