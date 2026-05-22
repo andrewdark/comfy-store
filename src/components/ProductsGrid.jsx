3e95b357b0d158decbd06e35db279173bb7987a8
andrewdark <samsonov.a@ukr.net>
2026-05-22 12:56:12 +0300

    add Section grid(raw data)

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -1,9 +1,22 @@
+import { MdDescription } from 'react-icons/md';
+import { useLoaderData, Link } from 'react-router-dom';
 
 const ProductsGrid = () => {
-    return (
-        <div>
+    const { products } = useLoaderData();
+    console.log(products);
 
+    return (
+        <div className='pt-12 grid gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+            {products.map(el => {
+                const { title, description } = el.attributes;
+                return <div key={el.id}>
+
+                    <h4>{title}</h4><p>{description}</p>
+                </div>
+            })}
         </div>
+
+
     );
 };
 
