ffd8937866629b93e8835d3f1dc4378725ce0d78
andrewdark <samsonov.a@ukr.net>
2026-06-08 20:30:58 +0300

    add searchParams to filtering on Prodact page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,16 +2,25 @@
 import { Filters, ProductsContainer, PaginationContainer } from '../components';
 import { customFetch } from '../http';
 
-const URL = '/products';
+const urlo = '/products';
 
 const allProductsQuery = (queryParams) => {
+
+    const { search, category, company, sort, price, shipping, page } = queryParams;
 
     return {
         queryKey: [
             'products',
+            search ?? '',
+            category ?? 'all',
+            company ?? 'all',
+            sort ?? 'a-z',
+            price ?? 100000,
+            shipping ?? false,
+            page ?? 1,
         ],
         queryFn: () =>
-            customFetch(URL, {
+            customFetch(urlo, {
                 params: queryParams,
             }),
     };
@@ -19,13 +28,15 @@ const allProductsQuery = (queryParams) => {
 
 
 export const loader = (queryClient) => async ({ request }) => {
-    const params = { search: '' };
+    const params = Object.fromEntries([
+        ...new URL(request.url).searchParams.entries(),
+    ]);
     const response = await queryClient.ensureQueryData(
         allProductsQuery(params)
     );
     const products = response.data.data;
     const meta = response.data.meta;
-    return { products, meta };
+    return { products, meta, params };
 };
 
 const Products = () => {
