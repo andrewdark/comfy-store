78345acdbb2b1794d9f13472e791fc90a4b027e7
andrewdark <samsonov.a@ukr.net>
2026-06-08 17:18:24 +0300

    Add Loader component

--- src/components/index.js
+++ src/components/index.js
@@ -14,3 +14,4 @@ export { default as ProductsContainer } from './ProductsContainer';
 export { default as PaginationContainer } from './PaginationContainer';
 export { default as ProductsGrid } from './ProductsGrid';
 export { default as ProductsList } from './ProductsList';
+export { default as Loading } from './Loading';
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,15 +1,20 @@
-import { Outlet } from 'react-router-dom';
-import { Header, NavBar } from '../components';
+import { Outlet, useNavigation } from 'react-router-dom';
+import { Header, NavBar, Loading } from '../components';
 
 const HomeLayout = () => {
+    const myNavy = useNavigation();
+    const isPageLoading = myNavy.state === 'loading';
+
     return (
         <div>
             <Header />
             <NavBar />
-            <section className='cstm-align-element py-20'>
-                <Outlet />
-            </section>
+            {isPageLoading ? <Loading /> :
+                <section className='cstm-align-element py-20'>
+                    <Outlet />
+                </section>
 
+            }
         </div>
     );
 };
