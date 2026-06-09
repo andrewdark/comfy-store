5ed7635fda0d338132a3ee070c0d95a7e14e202e
andrewdark <samsonov.a@ukr.net>
2026-06-09 14:23:20 +0300

    Add Redux store to the project,cartSlece  + wrap App

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App.jsx'
 
+import { ToastContainer } from 'react-toastify';
+import { store } from './store.js';
+import { Provider } from 'react-redux';
+
 createRoot(document.getElementById('root')).render(
-  <>
+  <Provider store={store}>
     <App />
-  </>,
+    <ToastContainer position='top-center' />
+  </Provider>,
 )
