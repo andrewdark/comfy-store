cff31e35a15ffd7be4aa7b0411f696a1510bc12c
andrewdark <samsonov.a@ukr.net>
2026-06-12 13:16:09 +0300

    Add orderList

--- src/components/OrdersList.jsx
+++ src/components/OrdersList.jsx
@@ -1,11 +1,47 @@
+import { useLoaderData } from 'react-router-dom';
 
 const OrdersList = () => {
+    const { orders, meta } = useLoaderData();
+
     return (
         <div className='mt-8'>
             <h4 className='mb-4 capitalize'>
-                total orders: 25
+                total orders: {meta.pagination.total}
             </h4>
+            <div className='overflow-x-auto'>
+                <table className='table table-zebra'>
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>Products</th>
+                            <th>Cost</th>
+                            <th className='hidden sm:block'>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map((order) => {
+                            const id = order.id;
+                            const { name, address, numItemsInCart, orderTotal, createdAt } =
+                                order.attributes;
+                            const date = createdAt
+                                .replace('T', ' ')      // Замінюємо "T" на пробіл
+                                .split('.')[0];         // Відрізаємо мілісекунди (все, що після крапки)
 
+                            return (
+                                <tr key={id}>
+                                    <td>{name}</td>
+                                    <td>{address}</td>
+                                    <td>{numItemsInCart}</td>
+                                    <td>{orderTotal}</td>
+                                    <td className='hidden sm:block'>{date}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
